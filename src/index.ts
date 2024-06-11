27dc2d685d6b79586f14eabbf19d8b215ce63ab2
quentinkrammer <bopatzable@gmail.com>
2024-06-11 08:31:23 +0200

    TRPC router plugin for express

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
+
+import { initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
+import z from 'zod'
+
+// created for each request
+const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = Awaited<ReturnType<typeof createContext>>;
+const t = initTRPC.context<Context>().create();
+const appRouter = t.router({
+    getUser: t.procedure.input(z.string()).query((opts) => {
+        opts.input;
+        return { id: opts.input, name: 'Bilbo' };
+    }),
+
+    // createUser: t.procedure
+    //     .input(z.object({ name: z.string().min(5) }))
+    //     .mutation(async (opts) => {
+    //         // use your ORM of choice
+    //         return await UserModel.create({
+    //             data: opts.input,
+    //         });
+    //     }),
+});
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }),
+);
+app.listen(3000);
