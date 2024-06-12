a379ba014d4b44bbca55ee49abd2602dcd128d38
quentinkrammer <bopatzable@gmail.com>
2024-06-12 09:31:10 +0200

    fix cors and http headers to allow cookie storage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,65 +7,65 @@ import { Strategy as LocalStrategy } from "passport-local";
 
 const MemoryStore = memoryStore(session);
 const store = new MemoryStore({
-  checkPeriod: 3600000,
+    checkPeriod: 3600000,
 });
 
 passport.use(
-  new LocalStrategy(function verify(username, password, cb) {
-    if (username === "Jim" && password === "123") {
-      console.log("Local Strategy - SUCCESS");
-      const user = { user: "Jim", role: "Admin", id: "42" };
-      return cb(null, user);
-    }
-    console.log("Local Strategy - FAILED");
-    return cb(null, false, { message: "Incorrect username or password." });
-  }),
+    new LocalStrategy(function verify(username, password, cb) {
+        if (username === "Jim" && password === "123") {
+            console.log("Local Strategy - SUCCESS");
+            const user = { user: "Jim", role: "Admin", id: "42" };
+            return cb(null, user);
+        }
+        console.log("Local Strategy - FAILED");
+        return cb(null, false, { message: "Incorrect username or password." });
+    }),
 );
 passport.serializeUser(function (user, cb) {
-  console.log("Serializer: ", user);
-  cb(null, user);
+    console.log("Serializer: ", user);
+    cb(null, user);
 });
 passport.deserializeUser(function (user, cb) {
-  console.log("DeserializeUser: ", user);
-  cb(null, user as Record<string, string>);
+    console.log("DeserializeUser: ", user);
+    cb(null, user as Record<string, string>);
 });
 
 const authRouter = express.Router();
 authRouter.post(
-  "/login",
-  passport.authenticate("local", {
-    failureMessage: true,
-    failureRedirect: "/notAuthed",
-  }),
-  function (req, res) {
-    console.log(
-      "Successfull authentication - User: ",
-      JSON.stringify(req.user),
-    );
-    res.redirect("/authed");
-  },
+    "/login",
+    passport.authenticate("local", {
+        failureMessage: true,
+        failureRedirect: "/notAuthed",
+    }),
+    function (req, res) {
+        console.log(
+            "Successfull authentication - User: ",
+            JSON.stringify(req.user),
+        );
+        res.redirect("/authed");
+    },
 );
 authRouter.get("/authed", function (req, res, next) {
-  // Question: Why is the user undefined? I need the user to do authorization :(
-  console.log("authRouter - User: ", JSON.stringify(req.user)); // req.user = undefined
-  res.json("Login successfull");
+    // Question: Why is the user undefined? I need the user to do authorization :(
+    console.log("authRouter - User: ", JSON.stringify(req.user)); // req.user = undefined
+    res.json("Login successfull");
 });
 authRouter.get("/notAuthed", function (req, res, next) {
-  res.json("Login failed");
+    res.json("Login failed");
 });
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    cookie: { maxAge: 3600000 },
-    store,
-    // not sure if this is needed
-    saveUninitialized: false,
-  }),
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        cookie: { maxAge: 3600000 },
+        store,
+        // not sure if this is needed
+        saveUninitialized: false,
+    }),
 );
 app.use(passport.authenticate("session"));
 app.use("/", authRouter);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,31 +6,32 @@ function App() {
 
   async function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-   
+
     const res = await fetch("http://localhost:3000/login", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      headers: { "Content-Type": "application/x-www-form-urlencoded", },
       body: `username=${e.currentTarget.username.value}&password=${e.currentTarget.password.value}`,
+      credentials: 'include'
     });
     const resBody = await res.json()
     setRes(resBody)
-    
+
   }
   return (
 
     <>
-    <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-      <label>
-        <span>Username</span>
-        <input type="text" name="username" required value="Jim" />
-      </label>
-      <label>
-        <span>Password</span>
-        <input type="password" name="password" required value={'123'} />
-      </label>
-      <button type="submit">Login</button>
-    </form>
-    <h1>Response: {res}</h1>
+      <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+        <label>
+          <span>Username</span>
+          <input type="text" name="username" required value="Jim" />
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="password" name="password" required value={'123'} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <h1>Response: {res}</h1>
     </>
   )
 }
