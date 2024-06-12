333634182567e4b1e41d0fe8f9ab73b73e209491
quentinkrammer <bopatzable@gmail.com>
2024-06-12 10:53:26 +0200

    Add TRPC with authedProcedure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
+import { TRPCError, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 import cors from "cors";
 import express from "express";
 import session from "express-session";
 import memoryStore from "memorystore";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import z from "zod"
 
 const MemoryStore = memoryStore(session);
 const store = new MemoryStore({
@@ -13,20 +16,16 @@ const store = new MemoryStore({
 passport.use(
     new LocalStrategy(function verify(username, password, cb) {
         if (username === "Jim" && password === "123") {
-            console.log("Local Strategy - SUCCESS");
             const user = { user: "Jim", role: "Admin", id: "42" };
             return cb(null, user);
         }
-        console.log("Local Strategy - FAILED");
         return cb(null, false, { message: "Incorrect username or password." });
     }),
 );
 passport.serializeUser(function (user, cb) {
-    console.log("Serializer: ", user);
     cb(null, user);
 });
 passport.deserializeUser(function (user, cb) {
-    console.log("DeserializeUser: ", user);
     cb(null, user as Record<string, string>);
 });
 
@@ -36,22 +35,49 @@ authRouter.post(
     passport.authenticate("local", {
         failureMessage: true,
         failureRedirect: "/notAuthed",
+        successRedirect: "/authed"
     }),
-    function (req, res) {
-        console.log(
-            "Successfull authentication - User: ",
-            JSON.stringify(req.user),
-        );
-        res.redirect("/authed");
-    },
 );
 authRouter.get("/authed", function (req, res, next) {
-    // Question: Why is the user undefined? I need the user to do authorization :(
-    console.log("authRouter - User: ", JSON.stringify(req.user)); // req.user = undefined
     res.json("Login successfull");
 });
 authRouter.get("/notAuthed", function (req, res, next) {
     res.json("Login failed");
+});
+
+
+const userSchema = z.object({
+    username: z.string(),
+    role: z.optional(z.literal('admin')),
+    userId: z.number()
+})
+const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => {
+    const user = process.env['NODE_ENV'] === 'development' ?
+        { username: 'DevEnvUser', 'role': 'admin', 'userId': 42 } : req.user
+
+    const { data } = userSchema.safeParse(user)
+    return ({
+        user: data
+    })
+}
+
+type Context = Awaited<ReturnType<typeof createContext>>;
+const t = initTRPC.context<Context>().create();
+
+const authedProcedure = t.procedure.use(async function isAuthed(opts) {
+    const { ctx } = opts;
+    if (!ctx.user) throw new TRPCError({ code: 'UNAUTHORIZED' });
+    // User is no longer undefined
+    return opts.next({ ctx: { user: ctx.user } })
+})
+
+const appRouter = t.router({
+    getFoo: authedProcedure.query(() => {
+        return "Foo";
+    }),
 });
 
 const app = express();
@@ -69,4 +95,11 @@ app.use(
 );
 app.use(passport.authenticate("session"));
 app.use("/", authRouter);
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }),
+);
 app.listen(3000);
