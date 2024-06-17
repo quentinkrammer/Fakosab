ab48483b71aa0533cd002cf944c01ba8e6ef7f2c
quentinkrammer <bopatzable@gmail.com>
2024-06-17 13:24:29 +0200

    add drizzle.development.config

--- backend/drizzle.development.config.ts
+++ backend/drizzle.development.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { type Config } from "drizzle-kit";
 
-const env = config({ path: ".env.production" });
+const env = config({ path: ".env.development" });
 
 export default {
   schema: "./src/db/schema.ts",
