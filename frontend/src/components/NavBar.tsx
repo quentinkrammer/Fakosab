9b358b8911d867deefdd1b0205083711aced4a54
quentinkrammer <bopatzable@gmail.com>
2024-07-01 14:16:25 +0200

    separate admin from user links

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,48 +1,52 @@
 import { css } from "goober";
 import { Avatar } from "primereact/avatar";
 import { Button } from "primereact/button";
+import { Divider } from "primereact/divider";
 import { Sidebar } from "primereact/sidebar";
 import { classNames } from "primereact/utils";
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import { PAGES, Pages } from "../constants";
 import { useActiveRoute } from "../hooks/useActiveRoute";
 import { useQueryMyUserData } from "../hooks/useQueryMyUserData";
 
-const LINK_CONFIG: Array<{
+type LinkConfig = {
   route: Pages;
   label: string;
   icon: string;
-  isAdminOnly?: boolean;
-}> = [
+};
+
+const USER_LINKS: LinkConfig[] = [
   { label: "Home", icon: "pi pi-home", route: PAGES.home },
   { label: "History", icon: "pi pi-clock", route: PAGES.history },
+];
+
+const ADMIN_LINKS: LinkConfig[] = [
   {
     label: "Users",
     icon: "pi pi-users",
     route: PAGES.users,
-    isAdminOnly: true,
   },
   {
     label: "Projects",
     icon: "pi pi-clipboard",
     route: PAGES.projects,
-    isAdminOnly: true,
   },
   {
     label: "Excel Export",
     icon: "pi pi-file-excel",
     route: PAGES.export,
-    isAdminOnly: true,
   },
 ];
 
-export function NavBar() {
+export const NavBar = memo(function NavBar() {
   const [visible, setVisible] = useState(false);
   const activeRoute = useActiveRoute();
   const userData = useQueryMyUserData();
   const username = userData.data?.username;
   const isAdmin = userData.data?.isAdmin;
+
+  const onLink = useCallback(() => setVisible(false), []);
 
   return (
     <>
@@ -66,32 +70,57 @@ export function NavBar() {
         onHide={() => setVisible(false)}
         showCloseIcon={false}
       >
-        {LINK_CONFIG.map(({ icon, label, route, isAdminOnly }) => {
-          if (isAdminOnly && !isAdmin) return;
-          return (
-            <Link
-              to={PAGES[route]}
-              style={{
-                textDecoration: "none",
-              }}
-              key={route}
-            >
-              <span
-                onClick={() => setVisible(false)}
-                className={classNames(styles.link, {
-                  [styles.activeLink]: activeRoute === route,
-                })}
-              >
-                <i className={icon} style={{ fontSize: "1.5rem" }}></i>
-                <h4 style={{ padding: "0" }}>{label}</h4>
-              </span>
-            </Link>
-          );
-        })}
+        {USER_LINKS.map((props) => (
+          <NavBarLink {...props} activeRoute={activeRoute} onClick={onLink} />
+        ))}
+        {isAdmin && (
+          <>
+            <Divider>{"Admin"}</Divider>
+            {ADMIN_LINKS.map((props) => (
+              <NavBarLink
+                {...props}
+                activeRoute={activeRoute}
+                onClick={onLink}
+              />
+            ))}
+          </>
+        )}
       </Sidebar>
     </>
   );
-}
+});
+
+type NavBarLinkProps = LinkConfig & {
+  activeRoute: ReturnType<typeof useActiveRoute>;
+  onClick?: () => void;
+};
+const NavBarLink = memo(function NavBarLink({
+  icon,
+  label,
+  route,
+  activeRoute,
+  onClick,
+}: NavBarLinkProps) {
+  return (
+    <Link
+      to={PAGES[route]}
+      style={{
+        textDecoration: "none",
+      }}
+      key={route}
+    >
+      <span
+        onClick={onClick}
+        className={classNames(styles.link, {
+          [styles.activeLink]: activeRoute === route,
+        })}
+      >
+        <i className={icon} style={{ fontSize: "1.5rem" }}></i>
+        <h4 style={{ padding: "0" }}>{label}</h4>
+      </span>
+    </Link>
+  );
+});
 
 const styles = {
   link: css({
